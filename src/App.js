72f28ba0e9ef96ad789bc94dbf59ff2028086df5
André Luiz Perensin <andreluizperensin@gmail.com>
2023-02-23 21:00:08 -0300

    added list to render posts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import React from "react";
 import Header from "./Header";
 import Post from "./Post";
 
+const posts = [
+  {
+    title: "Título da notícia 01",
+    subtitle: "Subtítulo da notícia 01",
+    likes: 0,
+  },
+  {
+    title: "Título da notícia 02",
+    subtitle: "Subtítulo da notícia 02",
+    likes: 2,
+  },
+  {
+    title: "Título da notícia 03",
+    subtitle: "Subtítulo da notícia 03",
+    likes: 20,
+  },
+  {
+    title: "Título da notícia 04",
+    subtitle: "Subtítulo da notícia 04",
+    likes: 7,
+  },
+  {
+    title: "Título da notícia 05",
+    subtitle: "Subtítulo da notícia 05",
+    likes: 13,
+  },
+];
+
 function App() {
   return (
     <>
@@ -11,34 +39,9 @@ function App() {
 
       <hr />
 
-      <Post
-        likes={0}
-        post={{
-          title: "Título da notícia 01",
-          subtitle: "Subtítulo da notícia 01",
-        }}
-      />
-      <Post
-        likes={5}
-        post={{
-          title: "Título da notícia 02",
-          subtitle: "Subtítulo da notícia 02",
-        }}
-      />
-      <Post
-        likes={4}
-        post={{
-          title: "Título da notícia 03",
-          subtitle: "Subtítulo da notícia 03",
-        }}
-      />
-      <Post
-        likes={20}
-        post={{
-          title: "Título da notícia 04",
-          subtitle: "Subtítulo da notícia 04",
-        }}
-      />
+      {posts.map(({ title, subtitle, likes }) => (
+        <Post key={title} post={{ title, subtitle }} likes={likes} />
+      ))}
     </>
   );
 }
