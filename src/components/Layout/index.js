336ef199f8d58900edb09242c7fd2d6413f36a42
André Luiz Perensin <andreluizperensin@gmail.com>
2023-02-27 20:32:35 -0300

    adjusted routes with exact param and added navbar with links to pages

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { Link, BrowserRouter } from "react-router-dom";
 
 import Header from "../Header";
 import Footer from "../Footer";
 import Routes from "../../Routes";
+import { Nav } from "./styles";
 
 export default function Layout({ onToggleTheme, selectedTheme }) {
   return (
-    <>
+    <BrowserRouter>
       <Header selectedTheme={selectedTheme} onToggleTheme={onToggleTheme} />
+      <Nav>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Posts</Link>
+      </Nav>
       <Routes />
       <Footer selectedTheme={selectedTheme} onToggleTheme={onToggleTheme} />
-    </>
+    </BrowserRouter>
   );
 }
