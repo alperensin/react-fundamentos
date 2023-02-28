bae23df371b9fa79c1f2ce9122e4837bbb35b85c
André Luiz Perensin <andreluizperensin@gmail.com>
2023-02-27 23:00:34 -0300

    using useHistory to navigate between pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import { Container } from "./styles";
 
 export default function Header({ selectedTheme, onToggleTheme }) {
+  const history = useHistory();
+
   return (
     <Container>
       <h1>JStack's Blog</h1>
       <button onClick={onToggleTheme} type="button">
         {selectedTheme === "dark" ? "🌞" : "🌚"}
+      </button>
+      <button onClick={() => history.push("/posts")} style={{ color: "#FFF" }}>
+        Posts
       </button>
     </Container>
   );
